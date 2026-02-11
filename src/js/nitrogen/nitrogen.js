ff98093a49a81f64f82baddae3685aa0e229366b
Cheval-R <rerisitdikov@gmail.com>
2026-02-11 08:39:54 +0300

    фикс

--- src/js/nitrogen/nitrogen.js
+++ src/js/nitrogen/nitrogen.js
@@ -133,7 +133,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function calculateDose() {
-    const zone = zoneElem.value;
+    const zone = "forestSteppe";
     const crop = cropElem.value;
     const tech = techElem.value;
     const nitrogenLevel = calculateNitrogenLevel();
@@ -145,68 +145,68 @@ document.addEventListener("DOMContentLoaded", () => {
   const DOSE_TABLE = {
     extensive: {
       very_high: {
-        taiga: { cereals: 25, row: 25, herbs: 0 },
+        // taiga: { cereals: 25, row: 25, herbs: 0 },
         forestSteppe: 17,
-        steppe: { cereals: 9, row: 9, herbs: 0 },
+        // steppe: { cereals: 9, row: 9, herbs: 0 },
       },
       high: {
-        taiga: { cereals: 12.5, row: 15, herbs: 0 },
+        // taiga: { cereals: 12.5, row: 15, herbs: 0 },
         forestSteppe: { cereals: 9, row: 9, herbs: 0 },
-        steppe: 0,
+        // steppe: 0,
       },
       medium: {
-        taiga: 0,
+        // taiga: 0,
         forestSteppe: 0,
-        steppe: 0,
+        // steppe: 0,
       },
       none: {
-        taiga: 0,
+        // taiga: 0,
         forestSteppe: 0,
-        steppe: 0,
+        // steppe: 0,
       },
     },
     simple: {
       very_high: {
-        taiga: { cereals: 35, row: 50, herbs: 50 },
+        // taiga: { cereals: 35, row: 50, herbs: 50 },
         forestSteppe: { cereals: 25, row: 25, herbs: 35 },
-        steppe: { cereals: 12.5, row: 25, herbs: 25 },
+        // steppe: { cereals: 12.5, row: 25, herbs: 25 },
       },
       high: {
-        taiga: { cereals: 25, row: 35, herbs: 35 },
+        // taiga: { cereals: 25, row: 35, herbs: 35 },
         forestSteppe: { cereals: 15, row: 15, herbs: 25 },
-        steppe: { cereals: 9, row: 15, herbs: 15 },
+        // steppe: { cereals: 9, row: 15, herbs: 15 },
       },
       medium: {
-        taiga: { cereals: 15, row: 15, herbs: 0 },
+        // taiga: { cereals: 15, row: 15, herbs: 0 },
         forestSteppe: 0,
-        steppe: 0,
+        // steppe: 0,
       },
       none: {
-        taiga: 0,
+        // taiga: 0,
         forestSteppe: 0,
-        steppe: 0,
+        // steppe: 0,
       },
     },
     intensive: {
       very_high: {
-        taiga: { cereals: 85, row: 105, herbs: 105 },
+        // taiga: { cereals: 85, row: 105, herbs: 105 },
         forestSteppe: { cereals: 70, row: 90, herbs: 90 },
-        steppe: { cereals: 50, row: 70, herbs: 70 },
+        // steppe: { cereals: 50, row: 70, herbs: 70 },
       },
       high: {
-        taiga: { cereals: 70, row: 80, herbs: 80 },
+        // taiga: { cereals: 70, row: 80, herbs: 80 },
         forestSteppe: { cereals: 50, row: 70, herbs: 70 },
-        steppe: { cereals: 35, row: 55, herbs: 50 },
+        // steppe: { cereals: 35, row: 55, herbs: 50 },
       },
       medium: {
-        taiga: { cereals: 50, row: 60, herbs: 60 },
+        // taiga: { cereals: 50, row: 60, herbs: 60 },
         forestSteppe: { cereals: 35, row: 50, herbs: 50 },
-        steppe: { cereals: 22.5, row: 40, herbs: 35 },
+        // steppe: { cereals: 22.5, row: 40, herbs: 35 },
       },
       none: {
-        taiga: 0,
+        // taiga: 0,
         forestSteppe: 0,
-        steppe: 0,
+        // steppe: 0,
       },
     },
   };
